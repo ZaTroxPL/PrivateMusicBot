89e5531e5f401d3c1f0dd20d96f60f9675d0eae6
Jakub Panczyszyn <zatroxkp@gmail.com>
2021-10-23 16:54:55 +0100

    Added an ability to skip the song, aka "next" it

--- PrivateMusicBot/Commands/General.cs
+++ PrivateMusicBot/Commands/General.cs
@@ -78,75 +78,52 @@ namespace PrivateMusicBot.Commands
 
             if (player.PlayerState == PlayerState.Playing || player.PlayerState == PlayerState.Paused)
             {
-                if (!string.IsNullOrWhiteSpace(searchResponse.Playlist.Name))
-                {
-                    foreach (var track in searchResponse.Tracks)
-                    {
-                        player.Queue.Enqueue(track);
-                    }
-
-                    await ReplyAsync($"Enqueued {searchResponse.Tracks.Count} tracks.");
-                }
-                else
-                {
-                    var track = searchResponse.Tracks.First();
-                    player.Queue.Enqueue(track);
-                    await ReplyAsync($"Enqueued: {track.Title}");
-                }
+                var track = searchResponse.Tracks.First();
+                player.Queue.Enqueue(track);
+                await ReplyAsync($"Enqueued: {track.Title}");
+                
             }
             else
             {
-                var track = searchResponse.Tracks.First();
-
-                if (!string.IsNullOrWhiteSpace(searchResponse.Playlist.Name))
-                {
-                    for (var i = 0; i < searchResponse.Tracks.Count; i++)
-                    {
-                        if (i == 0)
-                        {
-                            await player.PlayAsync(track);
-                            await ReplyAsync($"Now Playing: {track.Title}");
-                        }
-                        else
-                        {
-                            player.Queue.Enqueue(searchResponse.Tracks.ElementAt(i));
-                        }
-                    }
-
-                    await ReplyAsync($"Enqueued {searchResponse.Tracks.Count} tracks.");
-                }
-                else
-                {
-                    await player.PlayAsync(track);
-                    await ReplyAsync($"Now Playing: {track.Title}");
-                }
+                var track = searchResponse.Tracks.First();                
+                await player.PlayAsync(track);
+                await ReplyAsync($"Now Playing: {track.Title}");                
             }
             
-        }
-
-        private async Task OnTrackEnded(TrackEndedEventArgs args)
+        }     
+        
+        [Command("next")]
+        [Alias("n")]
+        public async Task NextAsync()
         {
-            if (args.Reason == TrackEndReason.Finished)
+            if (!lavaNode.HasPlayer(Context.Guild))
             {
+                await ReplyAsync("I'm not connected to a voice channel!");
                 return;
             }
 
-            var player = args.Player;
-            if (!player.Queue.TryDequeue(out var queueable))
+            var voiceState = Context.User as IVoiceState;
+            if (voiceState?.VoiceChannel == null)
             {
-                await player.TextChannel.SendMessageAsync("Queue completed! Please add more tracks to rock n' roll!");
+                await ReplyAsync("You must be connected to a voice channel!");
                 return;
             }
 
-            if (!(queueable is LavaTrack track))
+            var player = lavaNode.GetPlayer(Context.Guild);
+            if (voiceState.VoiceChannel != player.VoiceChannel)
             {
-                await player.TextChannel.SendMessageAsync("Next item in queue is not a track.");
+                await ReplyAsync("You must be connected to the same voice channel as me!");
                 return;
             }
 
-            await args.Player.PlayAsync(track);
-            await args.Player.TextChannel.SendMessageAsync(
-                $"{args.Reason}: {args.Track.Title}\nNow playing: {track.Title}");
+            if (player.Queue.Count == 0)
+            {
+                await ReplyAsync("There are no songs in the queue");
+                return;
+            }
+
+            await player.SkipAsync();
+            await ReplyAsync($"Playing the next song: {player.Track.Title}");
         }
 
         [Command("test")]
